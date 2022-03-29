1f7a7afa4e0a9d697612ef85c965672781a00f28
Emil Seyfullayev <67646591+EmilSeyfullayev@users.noreply.github.com>
2022-03-30 00:03:11 +0400

    Add files via upload

--- Chapter2.py
+++ Chapter2.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("Lenna_(test_image).png")
 
@@ -6,7 +7,10 @@ img_resized = cv2.resize(img, (200, 200))
 print(f'Original image shape {img.shape}')
 print(f'Resized image shape {img_resized.shape}')
 
+img_joined = np.hstack((img, img)) # vstack
+
 img_cropped = img[0:200, 0:200]
 cv2.imshow('Resized', img_resized)
 cv2.imshow('Cropped', img_cropped)
+cv2.imshow('Joined', img_joined)
 cv2.waitKey(0)
